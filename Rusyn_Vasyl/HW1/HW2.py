5bfc009592ab84658c4e6b729bda4fe8d0c6387f
Vasyl Rusyn <vasyarusynn@gmail.com>
2023-02-16 13:44:37 +0200

    delete folder Vasya Rusyn from main

--- Rusyn_Vasyl/HW1/HW2.py
+++
@@ -1,8 +0,0 @@
-a = float(input('Input first number'))
-b = float(input('Input second number'))
-
-print(f' A = {a}, b = {b}' 
-      f' A+B = {a+b}, A-B = {a-b},'
-      f' A*B = {a*b}, A/B = {a/b}'
-      f' A^B = {a**b}, A//B = {a//b}'
-      f' A%B = {a%b}')
