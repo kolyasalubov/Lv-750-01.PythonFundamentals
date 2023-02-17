083331186a1d2c4d6d44f42310233fca6c8a5b56
Sveta <svetlana.manoylo84@gmail.com>
2023-02-17 09:58:47 +0200

    Add homework into files: 1 and 2

---
+++ SvManoylo/HW1/1.py
@@ -0,0 +1,11 @@
+print('What is your name?')
+name = input()
+print('Hello, ' + name + '!', type(name))
+
+print('How old are you?')
+age = input()
+print('Your age is ' + age, type(age))
+
+print('Where do you live?')
+city = input()
+print('You live in ' + city, type(city))
---
+++ SvManoylo/HW1/2.py
@@ -0,0 +1,24 @@
+print('add your value a')
+a = int(input ())
+print('add your value b')
+b = int(input())
+print("Sum of value ") 
+print(a + b) 
+
+print("Dis of value ")
+print(a - b) 
+
+print("Mul of value ")
+print(a * b) 
+
+print("Div of value ")
+print(a / b) 
+
+print("Floor division of value ")
+print(a // b) 
+
+print("Exponent of value ")
+print(a ** b) 
+
+print("Modulus of value ")
+print(a % b) 
