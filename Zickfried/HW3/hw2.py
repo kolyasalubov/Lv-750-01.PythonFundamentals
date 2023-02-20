ccc073c8b62e06e14095771ca122c44fb2803d92
Grisha <yaunggrisha@gmail.com>
2023-02-20 23:32:30 +0200

    add HW3 with hw2 and hw3

---
+++ Zickfried/HW3/hw2.py
@@ -0,0 +1,21 @@
+a = 4532
+
+print("a =", a)
+
+#find product
+a = eval("4 * 5 * 3 *2")
+print("a =", a)
+
+#revers order
+a = 4532
+reverse_a = int(str(a)[::-1])
+print(reverse_a)
+
+#ascending order,sorted the numbers included in the given number
+sorted_digit_str = "".join(sorted(str(a)))
+sorted_a = int(sorted_digit_str)
+
+print(sorted_a)
+
+
+
