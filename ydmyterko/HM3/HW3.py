ac6568eb20235d6608a1ce290aa1444e74d580d8
Yuriy Dmyterko <yuriy.dmyterko@gmail.com>
2023-02-21 13:47:52 +0200

    03_Practical task GITHUB #3

--- ydmyterko/HM3/HW3.py
+++ ydmyterko/HM3/HW3.py
@@ -1,12 +1,22 @@
 x = 1
 y = 2
 
+#solution 1
 print("Init x:", x)
 print("Init y:", y)
 
 x = x+y
 y = x-y
 x = x-y
+print("Swap x:", x)
+print("Swap y:", y)
 
+#solution #2
+x = 1
+y = 2
+print("Init x:", x)
+print("Init y:", y)
+
+x,y = y,x
 print("Swap x:", x)
 print("Swap y:", y)
