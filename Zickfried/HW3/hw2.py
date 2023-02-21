8e685c63fb1dd92bd8276036a9a7502243fd4498
Grisha <yaunggrisha@gmail.com>
2023-02-21 17:40:52 +0200

    resolve problems in HW3 in hw2

--- Zickfried/HW3/hw2.py
+++ Zickfried/HW3/hw2.py
@@ -1,18 +1,20 @@
-a = 4532
+number = input("Enter 4 digits number:")
 
-print("a =", a)
+print(number)
+list_of_numbers = [int(i) for i in str(number)]
 
 #find product
-a = eval("4 * 5 * 3 *2")
-print("a =", a)
+product = 1
+for digit in number:
+    product *= int(digit)
+print(product)
 
 #revers order
-a = 4532
-reverse_a = int(str(a)[::-1])
+reverse_a = int(str(number)[::-1])
 print(reverse_a)
 
 #ascending order,sorted the numbers included in the given number
-sorted_digit_str = "".join(sorted(str(a)))
+sorted_digit_str = "".join(sorted(str(number)))
 sorted_a = int(sorted_digit_str)
 
 print(sorted_a)
