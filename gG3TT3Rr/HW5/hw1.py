11898065e886a7e65a61ae3e869e825b2d37153c
Nikita Konovalov <knvlvnkt@gmail.com>
2023-02-22 21:42:47 +0200

    added hw5

---
+++ gG3TT3Rr/HW5/hw1.py
@@ -0,0 +1,8 @@
+int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+float_list = []
+for num in int_list:
+    float_list.append(float(num))
+
+print("Int list:", int_list)
+print("Float list:", float_list)
