5c7d3d22c44283990a21416114e65f9179786fb6
Nikita Konovalov <knvlvnkt@gmail.com>
2023-02-24 13:01:25 +0200

    changed code in HW5

--- gG3TT3Rr/HW5/hw1.py
+++ gG3TT3Rr/HW5/hw1.py
@@ -1,8 +1,11 @@
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# int_list = (list(range(11)))
 
-float_list = []
-for num in int_list:
-    float_list.append(float(num))
+# float_list = []
+# for num in int_list:
+#     float_list.append(float(num))
 
-print("Int list:", int_list)
-print("Float list:", float_list)
+# print("Int list:", int_list)
+# print("Float list:", float_list)
+
+my_list = (list(range(33)))
+print(my_list[0])
