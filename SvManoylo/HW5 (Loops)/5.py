e1e05a14aa11e9ffcffd1f2f0862033f2a9907d8
Sveta <svetlana.manoylo84@gmail.com>
2023-02-27 13:16:07 +0200

    Add HW5 - 2part

--- SvManoylo/HW5 (Loops)/5.py
+++ SvManoylo/HW5 (Loops)/5.py
@@ -1,10 +1,10 @@
-# Task 1
-# list = (10, 20, 30 ,40, 50)
-# for item in list:
-#     print(float(item))
+# # Task 1
+list = (10, 20, 30 ,40, 50)
+for item in list:
+    print(float(item))
 
 
-#Task 2
+# #Task 2
 i1 = 0
 i2 = 1
 
@@ -16,26 +16,30 @@ while i2 > 0:
         break
 print("The End")
 
+#Task3 - first variant
+for i in range (100):
+    if i %2 == 0:
+        print(i)
 
+# #Task3 - second variant
+i = 0
+while i < 100:
+    print (i)
+    i += 2
 
+#Task 4
+for i in range (100):
+    if i %2 != 0:
+        print (i)
 
+for i in range (100):
+    if i %2 == 0:
+        continue
+    print (i)
 
-
-
-# for val in "string":
-#     if val == "i":
-#         continue
-# print(val)
-# print("The end")
-
-# for i in range(100):
-#  print (i)
-# else:
-#  print ("The End")
-# i = 1
-# while i < 100:
-#     print (i)
-#     i += 2
-# else:
-#     print ("The end")
-
+#Task 5
+numbers = [36,48,65]
+for i in numbers:
+    if i%2 != 0:
+        break
+    print ("List adding odd number")
