83f8179ab0c62098bc4fe80e71809b2f46735bdf
Sveta <svetlana.manoylo84@gmail.com>
2023-02-27 12:55:51 +0200

    Add HW5

---
+++ SvManoylo/HW5 (Loops)/5.py
@@ -0,0 +1,41 @@
+# Task 1
+# list = (10, 20, 30 ,40, 50)
+# for item in list:
+#     print(float(item))
+
+
+#Task 2
+i1 = 0
+i2 = 1
+
+while i2 > 0:
+    fib = i1 + i2
+    print(fib)
+    i1, i2 = i2, fib
+    if fib > 75:
+        break
+print("The End")
+
+
+
+
+
+
+
+# for val in "string":
+#     if val == "i":
+#         continue
+# print(val)
+# print("The end")
+
+# for i in range(100):
+#  print (i)
+# else:
+#  print ("The End")
+# i = 1
+# while i < 100:
+#     print (i)
+#     i += 2
+# else:
+#     print ("The end")
+
