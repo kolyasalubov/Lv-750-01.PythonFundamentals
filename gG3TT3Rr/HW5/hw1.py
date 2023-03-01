e2f41330987ac9bde2de062ec3d9a22a93cc902e
Nikita Konovalov <knvlvnkt@gmail.com>
2023-03-01 22:05:44 +0200

    added hw1 in HW6

--- gG3TT3Rr/HW5/hw1.py
+++ gG3TT3Rr/HW5/hw1.py
@@ -1,4 +1,4 @@
-#1
+# #1
 # int_list = (list(range(11)))
 
 # float_list = []
@@ -8,9 +8,13 @@
 # print("Int list:", int_list)
 # print("Float list:", float_list)
 
-#2
-n = int(input("Введите число n: "))
-a, b = 0, 1
-while a <= n:
-    print(a)
-    a, b = b, a+b
+# #2
+# n = int(input("Введите число n: "))
+# a, b = 0, 1
+# while a <= n:
+#     print(a)
+#     a, b = b, a+b
+
+
+for num in range(2,-5,-1):
+    print(num, end=", ")
