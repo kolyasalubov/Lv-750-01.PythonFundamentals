510a4e6e734301dc83ae73d2ab8655aa89925b77
Nikita Konovalov <knvlvnkt@gmail.com>
2023-03-10 22:25:05 +0200

    added HW7,8

--- gG3TT3Rr/HW5/hw1.py
+++ gG3TT3Rr/HW5/hw1.py
@@ -1,16 +1,16 @@
 # #1
 # int_list = (list(range(11)))
 
-float_list = []
-for num in int_list:
-    float_list.append(float(num))
+# float_list = []
+# for num in int_list:
+#     float_list.append(float(num))
 
-print("Int list:", int_list)
-print("Float list:", float_list)
+# print("Int list:", int_list)
+# print("Float list:", float_list)
 
-#2
-n = int(input("Введите число n: "))
-a, b = 0, 1
-while a <= n:
-    print(a)
-    a, b = b, a+b
+# #2
+# n = int(input("Введите число n: "))
+# a, b = 0, 1
+# while a <= n:
+#     print(a)
+#     a, b = b, a+b
