81152ff31a0fc8ca3f8112aff246e3766ab26b64
Grisha <yaunggrisha@gmail.com>
2023-03-21 15:47:39 +0200

    add in HW10 hw1 and in HW11 hw2

---
+++ Zickfried/HW10/hw1.py
@@ -0,0 +1,14 @@
+class Polygon():
+    pass
+
+class Rectangle(Polygon):
+    def __init__(self, side_1 = 0, side_2 = 0):
+        self.side_1 = side_1
+        self.side_2 = side_2
+        
+    def getRect_square(self):
+        return self.side_1 * self.side_2  
+    
+rect_sq = Rectangle(8, 6)
+
+print(rect_sq.getRect_square())     
