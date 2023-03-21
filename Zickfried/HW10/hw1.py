b39905b2538fc5a74db1b9f63319ec76ed38702b
Grisha <yaunggrisha@gmail.com>
2023-03-21 17:22:01 +0200

    add in HW10 hw1 another method

--- Zickfried/HW10/hw1.py
+++ Zickfried/HW10/hw1.py
@@ -14,4 +14,39 @@ class Rectangle(Polygon):
     
 rect_sq = Rectangle(8, 6)
 
-print(rect_sq.getRect_square())     
+print(rect_sq.getRect_square())  
+
+#####another method
+# class Polygon():
+#     """
+#     It's Polygon class
+#     """
+#     def __init__(self, no_of_sides):
+#         self.n = no_of_sides
+#         self.sides = [0 for i in range(no_of_sides)]
+    
+#     def inputSides(self):
+#         self.sides = [float(input(f"Enter side {str(i+1)}: "))
+#                                         for i in range(self.n)]
+            
+    
+#     def __str__(self):
+#         return f"It's {self.name}"   
+     
+# class Rectangle(Polygon):
+#     """
+#     Init rectangle and calculate square. 
+#     """
+#     def __init__(self):
+#         Polygon.__init__(self, 2)
+        
+#     def getRect_square(self):
+#         a, b = self.sides
+#         Square = a * b
+#         print(f"Square of Rectangle: {Square}")
+    
+# rect_sq = Rectangle()
+
+# rect_sq.inputSides() 
+
+# rect_sq.getRect_square()     
