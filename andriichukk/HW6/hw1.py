f0896190857583e88f148f06692fc4142f84a279
Viktoriia Andriichuk <v.andriichukk@gmail.com>
2023-03-22 10:27:10 +0100

    added HW6

---
+++ andriichukk/HW6/hw1.py
@@ -0,0 +1,7 @@
+for item in range(1, 10):
+    if item % 3 == 0:
+        print(item, "is number that divisible by 3")
+    elif item % 2 == 0:
+        print(item, "is even number")
+    else:
+        print(item, "is not divisible by 2 and 3")
